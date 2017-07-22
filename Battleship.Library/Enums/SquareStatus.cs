2cdf8086e2be8279ec0528319b1a802e7e8e2a3c
James Waudby <james.waudby@outlook.com>
2017-07-22 18:58:37 +0100

    Add square and grid classes. Add square status enum.

---
+++ Battleship.Library/Enums/SquareStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Battleship.Library.Enums
+{
+    public enum SquareStatus
+    {
+        Empty = 0,
+        Ship,
+        DeadShip,
+        Hit
+    }
+}
---
+++ Battleship.Library/Square.cs
@@ -0,0 +1,9 @@
+﻿using Battleship.Library.Enums;
+
+namespace Battleship.Library
+{
+    public class Square
+    {
+        public SquareStatus Status { get; set; }
+    }
+}

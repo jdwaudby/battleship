6584fbd070bdddd92a191eb7b8bca2eeaac857e4
James Waudby <james.waudby@outlook.com>
2017-07-23 16:26:32 +0100

    Add functions for setting and getting ship positions.

--- Battleship.Library/Services/Implementations/GridService.cs
+++ Battleship.Library/Services/Implementations/GridService.cs
@@ -1,4 +1,8 @@
-﻿using Battleship.Library.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Battleship.Library.Enums;
+using Battleship.Library.Models;
 using Battleship.Library.Services.Interfaces;
 
 namespace Battleship.Library.Services.Implementations
@@ -25,6 +29,41 @@ namespace Battleship.Library.Services.Implementations
             }
 
             return grid;
+        }
+
+        public IEnumerable<Point> GetShipPositions(Grid grid)
+        {
+            var shipPositions = new List<Point>();
+
+            for (int x = 0; x < grid.Squares.GetLength(0); x++)
+            {
+                for (int y = 0; y < grid.Squares.GetLength(1); y++)
+                {
+                    if (grid.Squares[x,y].Status == SquareStatus.Ship)
+                        shipPositions.Add(new Point(x, y));
+                }
+            }
+
+            return shipPositions;
+        }
+
+        public void SetRandomShipPositions(Grid grid, int maxX, int maxY, int ships)
+        {
+            var rand = new Random();
+            for (int i = 0; i < ships; i++)
+            {
+                int x = rand.Next(0, maxX), y = rand.Next(0, maxY);
+
+                Square square = grid.Squares[x, y];
+                while (square.Status == SquareStatus.Ship)
+                {
+                    x = rand.Next(0, maxX);
+                    y = rand.Next(0, maxY);
+                    square = grid.Squares[x, y];
+                }
+
+                square.Status = SquareStatus.Ship;
+            }
         }
     }
 }
--- Battleship.Library/Services/Interfaces/IGridService.cs
+++ Battleship.Library/Services/Interfaces/IGridService.cs
@@ -1,9 +1,13 @@
-﻿using Battleship.Library.Models;
+﻿using System.Collections.Generic;
+using System.Drawing;
+using Battleship.Library.Models;
 
 namespace Battleship.Library.Services.Interfaces
 {
     public interface IGridService
     {
         Grid Create(int width, int height);
+        IEnumerable<Point> GetShipPositions(Grid grid);
+        void SetRandomShipPositions(Grid grid, int maxX, int maxY, int ships);
     }
 }

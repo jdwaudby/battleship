ef448f2f3fcf852589f10bbc0b4de46ab6c3389f
James Waudby <james.waudby@blacklightsoftware.com>
2019-03-03 18:46:51 +0000

    Add GridCustomFormatter class.

---
+++ Battleship.Library/Formatters/GridCustomFormatter.cs
@@ -0,0 +1,25 @@
+﻿using Battleship.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Battleship.Library.Formatters
+{
+    public class GridCustomFormatter : IFormatProvider, ICustomFormatter
+    {
+        public object GetFormat(Type formatType)
+        {
+            if (formatType == typeof(ICustomFormatter))
+            {
+                return this;
+            }
+
+            return null;
+        }
+
+        public string Format(string format, object arg, IFormatProvider formatProvider)
+        {
+            return arg.ToString();
+        }
+    }
+}

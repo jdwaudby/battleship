c4b702b03565b7ec317b45582d2e720f8f8b2eba
James Waudby <james.waudby@outlook.com>
2020-08-18 17:36:45 +0100

    Add ShipPositioningException.cs

---
+++ Battleship.Library/Exceptions/ShipPositioningException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Battleship.Library.Exceptions
+{
+    public class ShipPositioningException : Exception
+    {
+        public ShipPositioningException()
+        {
+        }
+
+        public ShipPositioningException(string message)
+            : base(message)
+        {
+        }
+
+        public ShipPositioningException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}

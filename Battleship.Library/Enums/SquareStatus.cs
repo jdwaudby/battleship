78703469b01fc2dcba6957ef88c650f7c5e1218f
James Waudby <james.waudby@outlook.com>
2021-11-27 13:22:48 +0000

    Use bitwise left shift operator in square status enum

--- Battleship.Library/Enums/SquareStatus.cs
+++ Battleship.Library/Enums/SquareStatus.cs
@@ -3,12 +3,12 @@
     [Flags]
     public enum SquareStatus
     {
-        AircraftCarrier = 1,
-        Battleship = 2,
-        Cruiser = 4,
-        Submarine = 8,
-        Destroyer = 16,
-        Hit = 32,
+        AircraftCarrier = 1 << 0,
+        Battleship = 1 << 1,
+        Cruiser = 1 << 2,
+        Submarine = 1 << 3,
+        Destroyer = 1 << 4,
+        Hit = 1 << 5,
 
         Ship = AircraftCarrier | Battleship | Cruiser | Submarine | Destroyer
     }
